9920639efc2a4d70e644a58e4cf3344001fc0c6a
Ian <njugunaian6@gmail.com>
2025-06-11 13:18:50 -0700

    Corrected Third Task

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,7 +15,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         # Test the subtraction method
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
@@ -47,4 +47,5 @@ class TestSimpleCalculator(unittest.TestCase):
 if __name__ == '__main__':
     # Run the test suite
     unittest.main()
+
 
